f2beec59af7c19fbe839cab7952692babd23663b
Ryan Roxas <ryanaroxas@gmail.com>
2013-10-30 11:15:27 -0700

    Adds google map centered at San Francisco and removes basic yeoman html.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('barterApp', [])
+angular.module('barterApp', ['google-maps'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,10 @@ angular.module('barterApp')
       'AngularJS',
       'Karma'
     ];
+    $scope.center = {
+      latitude: 37.7833,
+      longitude: -122.4167
+    };
+    $scope.markers = [];
+    $scope.zoom = 8;
   });
