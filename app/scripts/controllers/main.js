ce89e40bcffff6b2d4763364db4991701ce8d6e2
Ryan Roxas <ryanaroxas@gmail.com>
2013-10-30 12:11:01 -0700

    Adds a zoom in button based on user's location.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,16 +1,37 @@
 'use strict';
 
 angular.module('barterApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $location) {
+    $scope.geolocationAvailable = navigator.geolocation ? true : false;
+    $scope.latitude = null;
+    $scope.longitude = null;
+    $scope.zoom = 11;
     $scope.center = {
       latitude: 37.7833,
       longitude: -122.4167
     };
     $scope.markers = [];
-    $scope.zoom = 8;
+    $scope.markerLat = null;
+    $scope.markerLng = null;
+    $scope.addMarker = function () {
+      $scope.markers.push({
+        latitude: parseFloat($scope.markerLat),
+        longitude: parseFloat($scope.markerLng)
+      });
+      $scope.markerLat = null;
+      $scope.markerLng = null;
+    };
+    $scope.findMe = function () {
+      if ($scope.geolocationAvailable) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          $scope.center = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          };
+          $scope.$apply();
+        }, function () {
+        });
+      }
+      $scope.zoom = 16;
+    };
   });
