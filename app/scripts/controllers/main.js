6273d65905499410f7813e5167d0349d23de679f
Ryan Roxas <ryanaroxas@gmail.com>
2013-10-30 18:26:06 -0700

    Sets up markers to be added on pageload.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('barterApp')
-  .controller('MainCtrl', function ($scope, $location) {
+  .controller('MainCtrl', function ($scope) {
     $scope.geolocationAvailable = navigator.geolocation ? true : false;
     $scope.latitude = null;
     $scope.longitude = null;
@@ -9,6 +9,21 @@ angular.module('barterApp')
     $scope.center = {
       latitude: 37.7833,
       longitude: -122.4167
+    };
+    $scope.markers = [];
+    $scope.markerLat = null;
+    $scope.markerLng = null;
+    $scope.items = [{latitude: 37.7837749,longitude:-122.40908739999998},{latitude: 37.7837749,longitude:-122.534},{latitude: 37.7837749,longitude:-122.403}];
+    $scope.addMarkers = function () {
+      for (var i = 0; i < $scope.items.length; i++){
+        var item = $scope.items[i];
+        $scope.markers.push({
+          latitude: parseFloat(item.latitude),
+          longitude: parseFloat(item.longitude)
+        });
+      }
+      $scope.markerLat = null;
+      $scope.markerLng = null;
     };
     $scope.findMe = function () {
       if ($scope.geolocationAvailable) {
@@ -22,5 +37,9 @@ angular.module('barterApp')
         });
       }
       $scope.zoom = 16;
+      $scope.addMarkers();
+    };
+    $scope.helloWorld = function(){
+      console.log("Hello World!");
     };
   });
