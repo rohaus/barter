d5f6c28beeb96cb386285c8322742701c54223b1
Ryan Roxas <ryanaroxas@gmail.com>
2013-12-20 09:16:18 -0800

    Add comment for readability

--- app/models/message.js
+++ app/models/message.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose');
+
+// Message Schema for messages in a conversation
 var messageSchema = new mongoose.Schema({
   'message': String,
   'from': String,
   'sentAt': { 'type': Date, 'default': Date.now }
 });
+
 var Message = mongoose.model('Message', messageSchema);
 
 module.exports = {
