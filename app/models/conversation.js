d259d1e0b0aaf6c0a59935bf27677ccbbbad1be1
Ryan Roxas <ryanaroxas@gmail.com>
2013-12-20 09:15:40 -0800

    Add comments to conversation.js for readability

--- app/models/conversation.js
+++ app/models/conversation.js
@@ -1,12 +1,14 @@
 var messageSchema = require('./message').messageSchema,
 mongoose = require('mongoose');
 
+// Conversation schema has messageSchema as a subdocument
 var conversationSchema = new mongoose.Schema({
   'requestingUser': { 'fbId': Number, 'name': String },
   'accepted': { 'type': Boolean, 'default': null },
   'createdAt': { 'type': Date, 'default': Date.now },
   'messages': [messageSchema]
 });
+
 var Conversation = mongoose.model('Conversation', conversationSchema);
 
 module.exports = {
