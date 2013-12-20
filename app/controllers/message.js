115c346af943918d462b2a7404a533b7ba7c0f4e
Ryan Roxas <ryanaroxas@gmail.com>
2013-12-19 16:01:39 -0800

    Remove unused functions and refactor code using utils file

--- app/controllers/message.js
+++ app/controllers/message.js
@@ -1,19 +1,20 @@
 var Post = require('../models/post').Post,
     Conversation = require('../models/conversation').Conversation,
-    Message = require('../models/message').Message;
+    Message = require('../models/message').Message,
+    utils = require('../utils'),
+    _ = require('lodash');
 
-var handleError = function(statusCode){
-  console.log(err);
-  res.send(statusCode);
-};
-
-var messages = function (req, res, next){
-  Post.find({}, function(err, messages){
-    if (err) { handleError(500); }
-    res.send(200, messages);
+// Function to add message to conversation
+var addMessageToConversation = function(req, post, message){
+  _.each(post.conversations, function(conversation){
+    if(conversation._id.equals(req.body._id)){
+      conversation.messages.push(message);
+      return;
+    }
   });
 };
 
+// Add a new conversation to the posting
 var sendNewConversation = function(req, res, next){
   var message = new Message({
     'message': req.body.message,
@@ -27,53 +28,35 @@ var sendNewConversation = function(req, res, next){
     'messages': [message]
   });
   Post.update({'_id': req.body._id}, {$push: {'conversations': conversation}}, function(err){
-    if (err) { handleError(500); }
+    utils.handleError(err, 500);
     res.send(201);
   });
 };
 
+// Add a new message to the conversation
 var sendMessage = function(req, res, next){
   var message = new Message({
     'message': req.body.message,
     'from': req.body.from
   });
   Post.findOne({'conversations._id': req.body._id}, function(err, post){
-    if (err) { handleError(500); }
-    for(var i = 0; i < post.conversations.length; i++){
-      var conversation = post.conversations[i];
-      if(conversation._id.equals(req.body._id)){
-        post.conversations[i].messages.push(message);
-        break;
-      }
-    }
-    post.save(function(err){
-      if (err) { handleError(500); }
-      res.send(201);
-    });
+    utils.handleError(err, 500);
+    addMessageToConversation(req, post, message);
+    saveChanges(res, post, 201, 500);
   });
 };
 
-// var deleteMessage = function(req, res, next){
-//   Conversation.messages.id(req.body._id);
-//   res.send(201);
-// };
-
+// Delete conversation in the posting
 var deleteConversation = function(req, res, next){
   Post.findOne({'conversations._id': req.params.id}, function(err, post){
-    if (err) { handleError(500); }
+    utils.handleError(err, 500);
     var conversation = post.conversations.id(req.params.id).remove();
-    post.save(function (err) {
-      if (err) { handleError(500); }
-      console.log('the sub-doc was removed');
-      res.send(204);
-    });
+    saveChanges(res, post, 204, 500);
   });
 };
 
 module.exports = {
-  messages: messages,
   sendNewConversation: sendNewConversation,
   sendMessage: sendMessage,
-  // deleteMessage: deleteMessage,
   deleteConversation: deleteConversation
 };
