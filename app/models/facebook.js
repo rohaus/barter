a15c98365680962d767cbba2058075640913d50f
Ryan Roxas <ryanaroxas@gmail.com>
2013-12-20 09:16:02 -0800

    Add comment for readability

--- app/models/facebook.js
+++ app/models/facebook.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
+
+// Facebook User Schema to keep track of fbId and name
 var facebookUserSchema = new mongoose.Schema({
   'fbId': String,
   'name': String
 });
+
 var FbUsers = mongoose.model('fbs', facebookUserSchema);
 
 module.exports = {
